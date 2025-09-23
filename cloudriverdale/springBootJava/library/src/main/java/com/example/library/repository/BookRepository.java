c8e4edec41b69f353845bfa06af7f2e34d6fa54e
jin <scene1993@gmail.com>
2025-09-23 23:23:41 +1000

    삭제

--- cloudriverdale/springBootJava/library/src/main/java/com/example/library/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.library.repository;
-
-import com.example.library.entity.Book;
-import org.springframework.data.repository.CrudRepository;
-
-public interface BookRepository extends CrudRepository<Book, Long> {
-}
