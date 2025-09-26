3e7dfdee80bcc60eb69142df962b906f262de5c3
jin <scene1993@gmail.com>
2025-09-27 01:28:46 +1000

    destructuring, underscore

--- rust/variables/src/main.rs
+++ rust/variables/src/main.rs
@@ -1,23 +1,5 @@
 fn main() {
-    // mut
-    let mut y = 5;
-    println!("mut y result : {y}");
-    y = 6;
-    println!("mut y result : {y}");
-
-    // shadowing
-    let x = 5;
-
-    let x = x + 1;
-
-    let x = x + 4;
-
-    {
-        let x = x * 2;
-        println!("The value of x in the inner scope is: {x}"); // 20
-    }
-
-    println!("The value of x is: {x}"); // 10
-
-
+    let tup = (500, 6.4, 1);
+    let (_x, _y, z) = tup;
+    println!("The value of y is : {z}");
 }
