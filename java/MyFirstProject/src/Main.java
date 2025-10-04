0fa50fd977e4273fcd00234e4bcd1ffe0717a6a5
jin <scene1993@gmail.com>
2025-10-04 15:21:04 +1000

    Mad libs game

--- java/MyFirstProject/src/Main.java
+++ java/MyFirstProject/src/Main.java
@@ -3,23 +3,30 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         
-        double width = 0;
-        double height = 0;
-        double area = 0;
+        Scanner scanner = new Scanner(System.in);
         
-        Scanner input = new Scanner(System.in);
+        String adjective1;
+        String noun1;
+        String adjective2;
+        String verb1;
+        String adjective3;
 
-        System.out.println("Enter the width :");
-        width = input.nextDouble();
+        System.out.print("Enter an adjective: (description word): ");
+        adjective1 = scanner.nextLine();
+        System.out.print("Enter an noun: (animal or person): ");
+        noun1 = scanner.nextLine();
+        System.out.print("Enter an verb end with -ing: (action word): ");
+        adjective2 = scanner.nextLine();
+        System.out.print("Enter an adjective: (description word): ");
+        verb1 = scanner.nextLine();
+        System.out.print("Enter an adjective: (description word): ");
+        adjective3 = scanner.nextLine();
 
-        System.out.println("Enter the height :");
-        height = input.nextDouble();
+        System.out.println("\nToday i went to a " + adjective1 + "zoo.");
+        System.out.println("In an exhibit, I saw a " + noun1 + ".");
+        System.out.println(noun1 + "was " + adjective2 + " and " + verb1 + "!");
+        System.out.println("I was + " + adjective3 + "!");
         
-        area = width * height;
-
-        System.out.println("The area of the rectangle is : " + area + "cm²");
-        
-        
-        input.close();
+        scanner.close();
     }
 }
