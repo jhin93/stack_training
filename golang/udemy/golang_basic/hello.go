81790eaab5f23a522d2dbbaa20ff1049684ff2a0
jin <scene1993@gmail.com>
2025-10-13 21:23:45 +1100

    37 finish

--- golang/udemy/golang_basic/hello.go
+++ golang/udemy/golang_basic/hello.go
@@ -2,15 +2,12 @@ package main
 
 import "fmt"
 
-type hotdog int
-
-var a int
-var b hotdog
-
 func main() {
-	b = 13
-	a = int(b)            // convert to int. Because b is hotdog
-	fmt.Printf("%T\n", b) // main.hotdog
-	fmt.Printf("%d\n", a) // 13
-	fmt.Printf("%d\n", b) // 13
+	x := 40
+	y := "James Bond"
+	z := true
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+	fmt.Println(x, y, z)
 }
