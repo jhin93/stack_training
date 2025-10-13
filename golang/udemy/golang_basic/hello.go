b11fd8f45fceaf02e87dccb17c2aeba3eae040d0
jin <scene1993@gmail.com>
2025-10-13 21:11:02 +1100

    convert to hotdog

--- golang/udemy/golang_basic/hello.go
+++ golang/udemy/golang_basic/hello.go
@@ -4,10 +4,12 @@ import "fmt"
 
 type hotdog int
 
+var a int
 var b hotdog
 
 func main() {
 	b = 13
+	a = int(b)            // convert to int. Because b is hotdog
 	fmt.Printf("%T\n", b) // main.hotdog
 	fmt.Printf("%d\n", b) // 43
 }
