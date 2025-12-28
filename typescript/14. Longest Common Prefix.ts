1a01c18f9802fc7b3df2d6db49a971a3fa0b83d5
jin <scene1993@gmail.com>
2025-12-28 16:43:02 +1100

    finish 14

---
+++ typescript/14. Longest Common Prefix.ts
@@ -0,0 +1,19 @@
+function longestCommonPrefix(strs: string[]): string {
+    if (!strs.length) return "";
+
+    strs.sort();
+
+    let firstArr: string[] = strs[0].split('');
+    let lastArr: string[] = strs[strs.length - 1].split('');
+
+    let result = "";
+
+    for (let i = 0; i < firstArr.length; i++) {
+        if (firstArr[i] === lastArr[i]) {
+            result += firstArr[i];
+        } else {
+            break;
+        }
+    }
+    return result;
+}
