d19ffce784f23c71ccae87fcff0e7e84b7061dd3
jin <scene1993@gmail.com>
2026-04-17 19:18:21 +1000

    round

---
+++ 32547 Unix Programming/week8/round.py
@@ -0,0 +1,22 @@
+# [user@sahara ~]$ python3
+# Python 3.14.3 (main, Feb 13 2026, 15:31:44) [GCC 15.2.1 20260209] on linux
+# Type "help", "copyright", "credits" or "license" for more information.
+# >>> 2 * 4
+# 8
+# >>> type(2 * 4)
+# <class 'int'>
+# >>> 2 / 4
+# 0.5
+# >>> int(0.3)
+# 0
+# >>> int(0.7)
+# 0
+# >>> round(0.6)
+# 1
+# >>> round(0.7)
+# 1
+# >>> round(0.5)
+# 0
+# >>> round(1.5)
+# 2
+# >>> 
