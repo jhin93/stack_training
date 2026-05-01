9e4bf887d6dfd0ad8a37570b4e60b8c2ef217333
jin <scene1993@gmail.com>
2026-05-01 19:34:10 +1000

    tutorial

---
+++ 32547 Unix Programming/week10/tutorial.py
@@ -0,0 +1,53 @@
+# #Write function here
+
+# def hello(person, other=None):
+#     print(f"Hello {person}.")
+#     if other is not None:
+#         print(f"This is {other}")
+#     print(f"My name is {other}")
+
+# hello("John", "Sally")
+# hello(other="John", person="Sally")
+# hello("John")
+
+# def hello_all(people):
+#     for person in people:
+#         print(f"Hello {person}")
+
+# hello_all(["John", "Sally", "Sam"])
+
+# def hello_all(*people):
+#     for person in people:
+#         print(f"Hello {person}")
+
+# hello_all("John", "Sally", "Sam")
+
+# def print_person(**person):
+#     print(f"Name: {person['name']}")
+#     print(f"Name: {person['age']}")
+
+# person = {"name": "Ryan", "age": 8}
+
+# print_person(name="Ryan", age=8)
+
+
+# def square(x):
+#     result = x * x
+#     print(f"The result is {result}")
+#     return result
+
+# r1 = square(4)
+# r2 = square(5)
+
+# print(f"result 1 is {r1}")
+# print(f"result 2 is {r2}")
+
+
+
+
+
+
+
+
+
+
